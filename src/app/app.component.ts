46ab99360c45fe60edd5a17cef841559a93e1729
Cyrus Mahdavi <cyrusmahdavi@lt00008079.usa.ccu.clearchannel.com>
2019-06-12 14:43:12 -0500

    get json working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  constructor(private http: HttpClient) {}
   title = 'untitled';
+  dataIn: any;
+  getAllHeroes(): void {
+    console.log('HERE!!!');
+    this.dataIn = 'hi';
+    this.http.get<JSON>('http://localhost:8080/api/advertising/all/').subscribe(
+
+      (response) => { console.log(response);
+                      console.log(JSON.stringify(response));
+                      this.dataIn = JSON.stringify(response); }
+    );
+
+  }
 }
